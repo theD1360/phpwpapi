4531689397d29618acbc678d219d2b39ff5bb7e4
Diego Alejos <lego.admin@gmail.com>
2012-11-05 10:32:52 -0600

    Update wp.api.class.php
    removed testing comments

--- wp.api.class.php
+++ wp.api.class.php
@@ -237,37 +237,4 @@ class wp_connect{
 
 }
 
-/*
-    Testing the class
-*/
-
-/*
-
-try{
-    $wp = new wp_connect("http://184.106.156.213/", "admin", "55G0ldP3ak");
-    
-
-   $attachments = $wp->apiRequest("attachments/get_attachments");
-   foreach($attachments as $k => $a){
-    if(isset($a->mime_type)){
-        echo $a->url."\n";
-        if(strstr($a->mime_type, "video")){
-         var_dump($wp->createPost(array(
-            "title" => "API test post {$a->title}",
-            "content" => '<div class="clearfix"><a href="http://184.106.156.213/wp-content/uploads/2012/09/beard_48_1_handlebar_mustache.png"><img class="alignleft  wp-image-40" title="beard_48_1_handlebar_mustache" src="http://184.106.156.213/wp-content/uploads/2012/09/beard_48_1_handlebar_mustache.png" alt="" width="322" height="154" /></a> This is a fancy post layout that I plan on using for the video section of the site. When the user clicks on the mustache it will open a video player lightbox and begin the playback.</div>
-<div><small><br />
-</small></div>
-<h5 style="text-align: justify;"><strong>Download:</strong> <a href="'.$a->url.'">Original</a> <a href="http://asdfsdf">flv</a> | <a href="http://asdfdsaf">1080</a> | <a href="http://asdfasdf">720</a> | <a href="http://asdfsadf">480</a> | <a href="http://asdfsad">mov</a> | <a href="http://asdfas">mpeg</a> | <a href="http://asdf">m4v</a></h5>
-',
-            "media" => array($a->id)
-        )));       
-        }
-    }
-   }
- 
-}catch(Exception $e){
-    echo $e->getMessage();
-}
-
-*/
 ?>
